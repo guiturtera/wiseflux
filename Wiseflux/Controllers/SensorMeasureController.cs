3e107bf25703e081ee2181c88969b170cf558f67
Guilherme Turtera <guiturtera@hotmail.com>
2023-11-21 22:29:04 -0300

    feat: added measures by month and day

--- Wiseflux/Controllers/SensorMeasureController.cs
+++ Wiseflux/Controllers/SensorMeasureController.cs
@@ -30,7 +30,7 @@ namespace Wiseflux.Controllers
         [HttpPost("add/{sensorId}/{sensorGuid}")]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ServiceResponse<SensorMeasure>))]
         [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ServiceResponse<object>))]
-        public async Task<ActionResult> AddSensor([FromRoute] int sensorId, [FromRoute] Guid sensorGuid, [FromBody] SensorMeasure sensorMeasure)
+        public async Task<ActionResult> AddSensorMeasure([FromRoute] int sensorId, [FromRoute] Guid sensorGuid, [FromBody] SensorMeasure sensorMeasure)
         {
             var result = await _sensorMeasureService.AddMeasure(sensorMeasure, sensorId, sensorGuid);
             Response.StatusCode = (int)result.Status;
@@ -46,9 +46,41 @@ namespace Wiseflux.Controllers
         [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ServiceResponse<object>))]
         [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(void))]
         [Authorize]
-        public async Task<ActionResult> GetAllSensors([FromRoute] int sensorId, [FromQuery] DateTime startDate, [FromQuery] DateTime endDate)
+        public async Task<ActionResult> GetAllSensorsMeasures([FromRoute] int sensorId, [FromQuery] DateTime startDate, [FromQuery] DateTime endDate)
         {
             var result = await _sensorMeasureService.GetSensorMeasures(sensorId, startDate, endDate, User);
+            Response.StatusCode = (int)result.Status;
+
+            return new JsonResult(result);
+        }
+
+        /// <summary>
+        /// Gets all user sensor
+        /// </summary>
+        [HttpGet("get/averageByMoth/{sensorId}")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ServiceResponse<IEnumerable<SensorMeasure>>))]
+        [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ServiceResponse<object>))]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(void))]
+        [Authorize]
+        public async Task<dynamic> GetAverageSensorsbByMonth([FromRoute] int sensorId, [FromQuery] DateTime startDate, [FromQuery] DateTime endDate)
+        {
+            dynamic result = await _sensorMeasureService.GetAvarageSensorMeasuresByMonth(sensorId, startDate, endDate, User);
+            Response.StatusCode = (int)result.Status;
+
+            return new JsonResult(result);
+        }
+
+        /// <summary>
+        /// Gets all user sensor
+        /// </summary>
+        [HttpGet("get/averageByDay/{sensorId}")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ServiceResponse<IEnumerable<SensorMeasure>>))]
+        [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ServiceResponse<object>))]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(void))]
+        [Authorize]
+        public async Task<dynamic> GetAverageSensorsbByDay([FromRoute] int sensorId, [FromQuery] DateTime startDate, [FromQuery] DateTime endDate)
+        {
+            dynamic result = await _sensorMeasureService.GetAvarageSensorMeasuresByDay(sensorId, startDate, endDate, User);
             Response.StatusCode = (int)result.Status;
 
             return new JsonResult(result);
--- Wiseflux/Services/SensorMeasureService.cs
+++ Wiseflux/Services/SensorMeasureService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Globalization;
+using System.Net;
 using System.Security.Claims;
 using Wiseflux.Data;
 using Wiseflux.Helpers;
@@ -43,13 +44,49 @@ namespace Wiseflux.Services
 
             if (formattedEndDate == DateTime.MinValue) formattedEndDate = DateTime.UtcNow; // if not specified (min value) returns all measures until the current datetime
 
-            List<SensorMeasure> measures = _db.SensorMeasures.Where(measure => 
+            var measures = _db.SensorMeasures.Where(measure =>
                 measure.SensorId == sensorId &&
                 measure.MeasureTime >= formattedStartDate &&
                 measure.MeasureTime <= formattedEndDate
             ).ToList();
 
             return new ServiceResponse<List<SensorMeasure>>(HttpStatusCode.OK, "Sucesso", measures);
+        }
+
+        private async Task<ServiceResponse<List<SensorMeasure>>> getAvarageDaysByFormat(int sensorId, DateTime startDate, DateTime endDate, ClaimsPrincipal user, string format)
+        {
+            var sensor = SensorHelper.GetSensorFromUser(_db, sensorId, user);
+
+            if (sensor == null) { return new ServiceResponse<List<SensorMeasure>>(HttpStatusCode.NotFound, "Sensor not found.", null); }
+
+            DateTime formattedStartDate = startDate; // if not specified comes as min value
+            DateTime formattedEndDate = endDate;
+
+            if (formattedEndDate == DateTime.MinValue) formattedEndDate = DateTime.UtcNow; // if not specified (min value) returns all measures until the current datetime
+
+            List<SensorMeasure> measures = _db.SensorMeasures.Where(measure =>
+                measure.SensorId == sensorId &&
+                measure.MeasureTime >= formattedStartDate &&
+                measure.MeasureTime <= formattedEndDate
+            ).ToList();
+
+            dynamic res = measures.GroupBy(measure =>
+                measure.MeasureTime.ToString(format)
+            ).Select(a =>
+                new SensorMeasure { MeasureValue = a.Average(a => a.MeasureValue), MeasureTime = DateTime.ParseExact(a.Key, format, CultureInfo.CurrentCulture) }
+            ).ToList();
+
+            return new ServiceResponse<List<SensorMeasure>>(HttpStatusCode.OK, "Sucesso", res);
+        }
+
+        public async Task<ServiceResponse<List<SensorMeasure>>> GetAvarageSensorMeasuresByMonth(int sensorId, DateTime startDate, DateTime endDate, ClaimsPrincipal user)
+        {
+            return await getAvarageDaysByFormat(sensorId, startDate, endDate, user, "MM/yy");
+        }
+
+        public async Task<ServiceResponse<List<SensorMeasure>>> GetAvarageSensorMeasuresByDay(int sensorId, DateTime startDate, DateTime endDate, ClaimsPrincipal user)
+        {
+            return await getAvarageDaysByFormat(sensorId, startDate, endDate, user, "dd/MM/yy");
         }
     }
 }
